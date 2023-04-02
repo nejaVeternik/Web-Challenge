638a0e3f4891668af037c154351dabf4c9b57b99
nejaVeternik <99610883+nejaVeternik@users.noreply.github.com>
2023-04-02 02:38:34 +0200

    API calls for products
    Implemented all API calls for products component

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -16,19 +16,53 @@ interface Product {
 export class ProductsComponent implements OnInit{
 
   products: any[] = [];
-  isShowing: boolean = true;
 
-  myName:string = 'a';
-
-  categories: string[] = []
+  categories: string[] = [];
 
   constructor(private httpService: HttpService) { }
 
   ngOnInit(): void {
+
     this.httpService.getProductList().subscribe(data => {
       this.products = data['products'];
       console.log(this.products);
+    });
+
+    /*this.httpService.getProduct('2').subscribe(data => {
+      console.log(data);
     })
+
+    this.httpService.searchProducts('phone').subscribe(data => {
+      console.log(data);
+    })
+
+    this.httpService.LimitAndSkipProducts('10', '10', '').subscribe(data => {
+      console.log(data);
+    })
+
+    this.httpService.getCategories().subscribe(data => {
+      console.log(data);
+    })
+
+    const postData = JSON.stringify({
+      title: 'BMW Pencil'
+    })
+
+    this.httpService.addProduct(postData).subscribe(data => {
+      console.log(data);
+    });
+
+    const postData = JSON.stringify({
+      title: 'BMW'
+    })
+
+    this.httpService.updateProduct(postData).subscribe(data => {
+      console.log(data);
+    })
+
+    this.httpService.deleteProduct('2').subscribe(data => {
+      console.log(data);
+    });*/
 
   }
 
--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, throwError } from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
@@ -13,5 +13,48 @@ export class HttpService {
   getProductList(): Observable<any> {
     return this.http.get('https://dummyjson.com/products');
   }
+
+  getProduct(product: string) {
+    const url = `https://dummyjson.com/products/${product}`;
+    return this.http.get(url);
+  }
+
+  searchProducts(product: string) {
+    const url = `https://dummyjson.com/products/search?q=${product}`;
+    return this.http.get(url);
+  }
   
+  LimitAndSkipProducts(limit: string, skip: string, select: string) {
+    const url = `https://dummyjson.com/products?limit=${limit}&skip=${skip}&select=${select}`;
+    return this.http.get(url);
+  }
+
+  getCategories() {
+    const url = `https://dummyjson.com/products/categories`;
+    return this.http.get(url);
+  }
+
+  addProduct(postData: any) {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    };
+    return this.http.post('https://dummyjson.com/products/add', postData, httpOptions)
+  }
+
+  updateProduct(postData: any) {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    };
+    return this.http.post('https://dummyjson.com/products/add', postData, httpOptions)
+  }
+
+  deleteProduct(product: string) {
+    const url = `https://dummyjson.com/products/${product}`;
+    return this.http.delete(url);
+  }
+
 }
